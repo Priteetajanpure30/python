237e95a12c4ec858b27d0fe19aad2f98b716273a
Pritee Tajanpure <122800124+Priteetajanpure30@users.noreply.github.com>
2023-02-02 13:48:51 +0530

    Create multutherding.py

---
+++ multutherding.py
@@ -0,0 +1,30 @@
+import threading;
+
+print("---------Marvellous Infosystem----------");
+print("Use of MULTITHREADING ");
+
+def fun(number):
+	for i in range(number):	
+		print(i);
+
+def gun(number):
+	for i in range(number):
+		print(i);
+
+def main():
+	number = 5;
+	
+	thread1 = threading.Thread(target = fun,args = (number,));
+	thread2 = threading.Thread(target = gun,args = (number,));
+	
+	thread1.start();
+	thread2.start();
+
+	thread1.join();
+	thread2.join();
+
+
+
+
+if __name__ == "__main__":
+	main();
