0888a3ca59421f4588d6f9c9558eed7f1ae46cc1
Pritee Tajanpure <122800124+Priteetajanpure30@users.noreply.github.com>
2023-02-02 13:41:43 +0530

    Create dic.py

---
+++ dic.py
@@ -0,0 +1,16 @@
+dict = {};
+print("Empty dictonary : ",dict);
+
+
+#Dictionary holds key:value pair
+dict = {1:'prachi',2:'manu',3:'gauri',4:'jayesh',5:'neha'};
+print("Dictonary with some key:value pair : ",dict);
+
+
+#Values in a dictionary can be of any datatype and can be duplicated, whereas keys can’t be repeated and must be immutable. 
+dict = {1:'p',1:'m',3:'g',4:'j',3:'n'};
+print("Dictonary with some repeated key:value pair : ",dict);
+
+
+dict = {1:'pr',2:'manu',3:'pr',4:'jayesh',5:'neha'};
+print("Dictonary with some key:repeated value pair : ",dict);
